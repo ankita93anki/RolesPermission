fa247b52f7ba388f03a9b01643bea0680460a8fe
Ankita Singh <ankitasingh7639369@gmail.com>
2025-02-19 00:01:05 +0530

    Permission list

--- resources/views/permissions/list.blade.php
+++ resources/views/permissions/list.blade.php
@@ -6,7 +6,7 @@
             </h2>
             <a href="{{ route('permissions.create')}}" class="bg-slate-700 text-sm rounded-md text-white px-5 py-3">Create</a>
        </div>
-    </x-slot>      
+    </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -14,10 +14,10 @@
             <table class="w-full">
                 <thead class="bg-gray-50">
                     <tr class="border-b">
-                        <th class="px-6 py-3 text-left">#</th class="px-6 py-3 text-left">
+                        <th class="px-6 py-3 text-left" width="60">#</th class="px-6 py-3 text-left">
                         <th class="px-6 py-3 text-left">Name</th>
-                        <th class="px-6 py-3 text-left">Created</th>
-                        <th class="px-6 py-3 text-center">Action</th>
+                        <th class="px-6 py-3 text-left" width="120">Created</th>
+                        <th class="px-6 py-3 text-center" width="180">Action</th>
                     </tr>
                 </thead>
                 <tbody class="bg-white">
@@ -31,10 +31,10 @@
                                      {{ $permission->name}}
                                 </td>
                                 <td class="px-6 py-3 text-left">
-                                      {{ $permission->created_at}}
+                                      {{ \carbon\carbon::parse($permission->created_at)->format('d M, Y')}}
                                 </td>
                                 <td class="px-6 py-3 text-center">
-                                       <a href="{{ route('permissions.create')}}" class="bg-slate-700 text-sm rounded-md text-white px-5 py-3">Edit</a>|<a href="{{ route('permissions.create')}}" class="bg-slate-700 text-sm rounded-md text-white px-5 py-3">Delete</a>
+                                       <a href="{{ route('permissions.create')}}" class="bg-slate-700 text-sm rounded-md text-white px-5 py-3">Edit</a>|<a href="{{ route('permissions.create')}}" class="bg-red-700 text-sm rounded-md text-white px-5 py-3">Delete</a>
                                 </td>
                             </tr>
                        @endforeach
