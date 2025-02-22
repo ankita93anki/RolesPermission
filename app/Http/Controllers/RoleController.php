f19772120be9917e32db16da5dfd84f651f36421
Ankita Singh <ankitasingh7639369@gmail.com>
2025-02-22 23:51:46 +0530

    Roles Permission

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    //This method will show roles page
+    public function index()
+    {
+
+    }
+
+    //this method create page
+    public function create()
+    {
+       return view('roles.create');
+    }
+
+    //this method store role
+    public function store()
+    {
+
+    }
+
+    //this method edit page
+    public function edit()
+    {
+
+    }
+}
